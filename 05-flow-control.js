1740361abfde600ce27444ae06a4cfabb83eb3f7
JJiwonn <163959530+JJiwonn@users.noreply.github.com>
2024-05-23 16:07:19 +0900

    20240523 commit

--- 05-flow-control.js
+++ 05-flow-control.js
@@ -3,19 +3,14 @@
 // For문 버전
 // While문 버전
 console.log("For문 구구단")
-for(let dan = 2; dan <= 9; dna++) {
+for(let dan = 2; dan <= 9; dan++) {
     for(let i = 1; i < 10; i++) {
-        console.log("dan * " + i + " = " + dan*i);
+        console.log(dan + " * " + i + " = " + (dan*i));
     }
 }
 
 
-console.log("While문 구구단")
-let i = 1;
-while (i <= 9) {
-    console.log("2 * " + i + " = " + 2*i);
-    i++;
-}
+
 
 // 연습문제 2. 아래와 같이 출력하는 프로그램을 만들어 봅시다
 /*
